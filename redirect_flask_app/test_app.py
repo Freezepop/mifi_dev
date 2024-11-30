97ef8c309bba3ab952c5487910d9ebd440e863d5
Freezepop <Cyberchicken@yandex.ru>
2024-11-30 19:37:18 +0300

    add Python zalipuha

--- redirect_flask_app/test_app.py
+++ redirect_flask_app/test_app.py
@@ -5,15 +5,15 @@ from redirect_flask_app import app
 
 @pytest.fixture
 def client():
-    app.config['TESTING'] = True
+    app.config["TESTING"] = True
     return app.test_client()
 
 def test_redirect_with_url(client):
-    response = client.get('/redirect?url=https://example.com')
+    response = client.get("/redirect?url=https://example.com")
     assert response.status_code == 302
-    assert response.location == 'https://example.com'
+    assert response.location == "https://example.com"
 
 def test_redirect_without_url(client):
-    response = client.get('/redirect')
+    response = client.get("/redirect")
     assert response.status_code == 400
     assert b'No URL for redirection' in response.data
